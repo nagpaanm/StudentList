0f439c1504946a0b31c7225ece15cf76128e0e7a
Anmol-Nagpal <= nagpaanm@sheridancollege.ca>
2019-02-05 15:55:30 -0500

    Added maxHours property

--- src/studentslist/Student.java
+++ src/studentslist/Student.java
@@ -15,7 +15,9 @@ public class Student {
     private String name;
     private int id;
     private String DoB;
- 
+    
+    private int maxHours;
+    
     public Student()
     {
         name = "Not Set";
